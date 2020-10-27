27c3c5c73f7c4dc5f794a6916269d4acdf0dbd16
Amir2170 <amirrezaamp2170@gmail.com>
2020-10-27 23:41:45 +0330

    changes settings.py

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,5 +2,15 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
-REPO_URL = 
+REPO_URL = 'https://github.com/Amir2170/newspaper.git'
+
+
+def deploy():
+	site_folder = f'/home/{env.user}/sites/{env.host}'
+	source_folder = site_folder + '/source'
+	_create_directory_structure_if_necessary(site_folder)
+	_get_latest_source(source_folder)
+	_update_settings(source_foler, env.host)
+	_update_virtualenv(source_folder)
+	_update_database(source_folder)
 
--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'newspaper_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(os.path.dirname(BASE_DIR),'../database/db.sqlite3')
     }
 }
 
