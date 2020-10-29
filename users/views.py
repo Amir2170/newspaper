fda416541cd11ede766de94072a1b29c657756a8
Amir2170 <amirrezaamp2170@gmail.com>
2020-10-30 01:11:24 +0330

    add csrf_required

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import PasswordResetView
+from django.views.decorators.csrf import requires_csrf_token
+from django.utils.decorators import method_decorator
 
 from .forms import CustomUserCreationForm
 
 
+@method_decorator(requires_csrf_token, name='dispatch')
 class SignUpView(CreateView):
 	
 	form_class = CustomUserCreationForm
