e99c45b4e6a970539cb096d5a4a8de00e818ea20
Amir2170 <amirrezaamp2170@gmail.com>
2020-10-30 01:42:39 +0330

    two consecutive decorators for csrf token

--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.views import PasswordResetView
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, requires_csrf_token
 from django.utils.decorators import method_decorator
 
 from .forms import CustomUserCreationForm
 
-
-@method_decorator(csrf_exempt, name='dispatch')
+csrf_token_decorators = [requires_csrf_token, csrf_exempt]
+@method_decorator(csrf_token_decorators, name='dispatch')
 class SignUpView(CreateView):
 	
 	form_class = CustomUserCreationForm
