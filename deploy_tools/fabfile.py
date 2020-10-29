7b9b5283b5dd69248b6e59f1523114813684c2d7
Amir2170 <amirrezaamp2170@gmail.com>
2020-10-29 13:11:32 +0330

    hidden secret_key

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -2,7 +2,48 @@ from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run
 import random
 
+
 REPO_URL = 'https://github.com/Amir2170/newspaper.git'
+
+def _create_directory_structure_if_necessary(site_folder):
+	for subfolder in ('database', 'virtualenv', 'source'):
+		run(f'mkdir -p {site_folder}/{subfolder}')
+
+
+def _get_latest_source(source_folder):
+	if exists(source_folder + '/newspaper/.git'):
+		run(f'cd {source_folder}/newspaper && git fetch')
+	else:
+		run(f'git clone {REPO_URL} {source_folder}')
+	current_commit = local("git log -n 1 --format=%H", capture=True)
+	run(f'cd {source_folder}/newspaper && git reset --hard {current_commit}')
+
+
+def _update_settings(source_foler, site_name):
+	settings_path = source_folder + '/newspaper/newpaper_project/settings.py'
+	sed(settings_path, "DEBUG=True", "DEBUG=False")
+	sed(settings_path,
+		"ALLOWED_HOSTS = .+$",
+		f'ALLOWED_HOSTS = [{site_name}]',
+	)
+	secret_key_file = source_folder + '/newspaper/newspaper_project/secret_key.py'
+	if not exists(secret_key_file):
+		chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+		key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+		append(secret_key_file, f'SECRET_KEY = "{key}"')
+	append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+def _update_virtualenv(source_folder):
+	virtualenv_folder = source_folder + '/../virtualenv'
+	if not exists(virtualenv_folder +'/bin/pip3.8'):
+		run(f'python3.8 -m venv {virtualenv_folder}')
+	run(f'{virtualenv_folder}/bin/pip3.8 install -r {source_folder}/newspaper/requirments.txt')
+		
+
+def _update_database(source_folder):
+	run(f'cd {source_folder}'
+		'&& ../virtualenv/bin/python3.8 manage.py migrate --noinput'
+	)
 
 
 def deploy():
--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-((!6%g!5k%ye(1+6g$7ny^q*48kcau(tdinj_)c2rob+)=+6-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
